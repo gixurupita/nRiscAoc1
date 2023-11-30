98167200d578097991aa80b9fbf7fea9f647a93b
manel018 <emanuel.fonseca81@hotmail.com>
2023-11-30 18:59:55 -0300

    "arquivo com lista de instruções em binário adicionado"
    Adicionado: modulo da memoria de instruções (com printf dos valores) e arquivo .bat com toda a sequência de instruções do programa em binário.

---
+++ MemoriaInstrucoes.v
@@ -0,0 +1,26 @@
+module MemoriaInstrucoes (Endereco, Instrucao, clock);
+    input wire clock;   //Sinal de clock
+    input wire [7:0] Endereco;
+    output reg [7:0] Instrucao; //Instrução de saída
+
+    reg [7:0] MemInstrucoes [0:255];    //Declaração o reg da Memória
+
+    integer i;
+    initial begin
+        //Lê o arquivo e grava as instruções na memória
+        $readmemb("instrucoes.dat", MemInstrucoes, 0, 255);
+
+        //  ===== INÍCIO: Prints de teste =====
+        $display("Valores binarios lidos e gravados na Memória de Instruções:");
+        for(i = 0; i < 71; i =  i + 1)
+            $display("memInst[%0d] %b", i, MemInstrucoes[i]);
+        //  ===== FIM: Prints de teste =====
+    end
+
+    always @(negedge clock)
+    begin
+        //Atribui a respectiva instrução à saída
+        Instrucao = MemInstrucoes[Endereco];  
+    end
+endmodule
+
